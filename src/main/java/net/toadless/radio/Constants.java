c46387aedcc59d7a24aa50fbe4e17722772e7286
toad <thomasmcdonald2027@gmail.com>
2022-02-20 17:53:41 +0000

    🙈 Update Constants
    No longer a copied file from Monke! 🙈

--- src/main/java/net/toadless/radio/Constants.java
+++ src/main/java/net/toadless/radio/Constants.java
@@ -4,9 +4,9 @@ import java.awt.*;
 
 public class Constants
 {
-    public static final String DEFAULT_BOT_PREFIX = "m!";
+    public static final String DEFAULT_BOT_PREFIX = "r!";
 
-    public static final Color EMBED_COLOUR = Color.decode("#C29957");
+    public static final Color EMBED_COLOUR = Color.decode("#E0A51F");
 
     public static final String VERSION = "1.0.0";
 
